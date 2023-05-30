eaa395c11f887d97ccc94e88a0ad1b7b51b9b2a6
Rohit_J <87205506+Rohit-J99@users.noreply.github.com>
2023-05-30 14:37:03 +0530

    jenkins file updated

--- scripts/Demo.py
+++ scripts/Demo.py
@@ -1,4 +1,22 @@
 import subprocess
+import argparse
+import os
 
-subprocess.run("opa test ./policies/policy1 -v",shell=True)
-subprocess.run("conftest verify --policy ./policies/policy1 --output=table",shell=True)
+parser = argparse.ArgumentParser()
+parser.add_argument("--Arg1")
+parser.add_argument("--Arg2")
+Args=parser.parse_args()
+
+print(os.environ["NEW_VERSION"])
+
+sub1=subprocess.run("opa test ./policies/policy1 -v",shell=True,capture_output=True,text=True)
+sub2=subprocess.run("conftest verify --policy ./policies/policy1 --output=table",shell=True,capture_output=True,text=True)
+
+if(sub1.returncode==0 and sub2.returncode==0):
+    print(sub1.stdout,sub2.stdout)
+    print('Success!')
+else:
+    print('Failure!')
+
+print(Args)
+
---
+++ scripts/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+if(sub1.returncode==0 and sub2.returncode==0):
+#     print(sub1.stdout,sub2.stdout)
+#     print('Success!')
+# else:
+#     print('Failure!')
